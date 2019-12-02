ce220813628469c9df77e5592fa3bd69a06efc55
India Rodriguez <carnes.s.ia17@gmail.com>
2019-12-01 22:53:14 -0700

    beginning to refactor the bookmarks API to use postgresql and express together. Created migration, seed, and test files. Also created service file.

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: "0b401ad2-222c-45f3-9ef9-385433fdb841",
+    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,36 @@
+function makeBookmarksArray() {
+    return [
+        {
+            id: 1,
+            title: 'Test title 1',
+            url: 'Test url 1',
+            description: 'Test description 1.',
+            rating: 4
+        },
+        {
+            id: 2,
+            title: 'Test title 2',
+            url: 'Test url 2',
+            description: 'Test description 2.',
+            rating: 5
+        },
+        {
+            id: 3,
+            title: 'Test title 3',
+            url: 'Test url 3',
+            description: 'Test description 3.',
+            rating: 3
+        },
+        {
+            id: 4,
+            title: 'Test title 4',
+            url: 'Test url 4',
+            description: 'Test description 4.',
+            rating: 2
+        },
+    ];
+}
+
+module.exports = {
+    makeBookmarksArray,
+}
